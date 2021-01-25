61304a3378928a9fa37811d38f186152a0d4c5f1
Gecko05 <jacacent@gmail.com>
2021-01-25 09:34:49 -0600

    ex3_1

--- chap1/chap3/main.c
+++
@@ -1,9 +0,0 @@
-bool strneq(const char* s1, const char* s2, int){
-    while ( *s1 != 0 && *s2 != 0 && n > 0){
-        if (*s1++ != *s2++){
-            return false;
-        }
-        n--;
-    }
-    return (*s1 == *s2);
-}
