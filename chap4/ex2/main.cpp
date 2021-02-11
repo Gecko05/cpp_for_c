fce7cda39c08f45b1827c77723b81b6045d635d9
Gecko05 <jacacent@gmail.com>
2021-02-11 14:17:17 -0600

    update

---
+++ chap4/ex2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct point{
+    float x;
+    float y;
+    float z;
+};
+
+int main(int argc, char *argv[])
+{
+    struct point base = {0.1, 0.2, 0.5};
+}
---
+++ chap4/ex3/main.cpp
@@ -0,0 +1,14 @@
+struct brother;
+struct sister;
+
+struct brother {
+    char name[20];
+    int age;
+    struct sister sib;
+} a;
+
+struct sister {
+    char name[20];
+    int age;
+    struct brother sib;
+} b;
