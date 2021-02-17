ad1836ec8b3a034badbc98c9b1cdfa8341377e81
Gecko05 <jacacent@gmail.com>
2021-02-17 09:18:47 -0600

    ex4_9

---
+++ chap4/ex9/main.cpp
@@ -0,0 +1,10 @@
+struct a {
+    int i, j, k;
+};
+
+// This class is not useful because data members
+// are private by default. We need to add the public
+// tag in order to be able to use its data members.
+class a{ 
+    int i, j, k;
+};
