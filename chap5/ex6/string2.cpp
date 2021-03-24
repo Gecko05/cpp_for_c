57e1c165a7310af99e68cc5dece7ccec0951502c
Gecko05 <jacacent@gmail.com>
2021-03-24 10:26:48 -0600

    ex6

--- chap5/ex6/string2.cpp
+++ chap5/ex6/string2.cpp
@@ -22,6 +22,9 @@ class my_string {
             cout << s << endl;
         }
         void concat(const my_string& a, const my_string& b);
+        int my_string::strcmp(const my_string& s1);
+        void my_string::strrev();
+        void my_string::print(int n);
     private:
         char* s;
         int len;
@@ -64,4 +67,42 @@ void my_string::concat(const my_string& a, const my_string& b)
     s = new char[len + 1];
     assert(s != 0);
     strcpy(s, temp);
+}
+
+// strcmp is negative if s < s1,
+//      is 0 if s == s1,
+//      and is positive if s > s1
+//      where s is the implicit argument
+
+int my_string::strcmp(const my_string& s1)
+{
+    for (int i = 0; i < this->len ; i++)
+    {
+        if (this->s[i] < s1.s[i]){
+            return -1;
+        }
+        else if (this->s[i] > s1.s[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// strrev reverses the my_string
+void my_string::strrev(){
+    char* sr = new char[len];
+    memcpy(sr, this->s, this->len);
+    for (int i = 0; i < len; i++)
+    {
+        this->s[len-i-1] = sr[i];
+    }
+    delete []sr;
+}
+
+// print overloaded to print the first n characters
+void my_string::print(int n){
+    for (int i = 0; (i < n) && (i < len); i++)
+    {
+        cout << this->s[i];
+    }
 }
